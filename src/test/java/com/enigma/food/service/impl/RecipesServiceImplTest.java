f33d554484d5fdaeddf1cd7b4749f87f02c9bd92
Ikhsan Burhan <ikhsanburhanalyaqin28@gmail.com>
2024-07-22 19:10:41 +0700

    [FEATURES] AddRecipeServiceImplTestDto C

--- src/test/java/com/enigma/food/service/impl/RecipesServiceImplTest.java
+++ src/test/java/com/enigma/food/service/impl/RecipesServiceImplTest.java
@@ -1,8 +1,14 @@
 package com.enigma.food.service.impl;
 
+import com.enigma.food.model.Ingridients;
+import com.enigma.food.model.Items;
 import com.enigma.food.model.Recipes;
+import com.enigma.food.repository.IngridientsRepository;
 import com.enigma.food.repository.RecipesRepository;
 import com.enigma.food.service.ValidationService;
+import com.enigma.food.utils.dto.ItemsCreateDto;
+import com.enigma.food.utils.dto.RecipeCreatesDTO;
+import jakarta.validation.Valid;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,9 +21,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class RecipesServiceImplTest {
@@ -26,8 +32,12 @@ public class RecipesServiceImplTest {
     private RecipesRepository repository;
 
     @Mock
+    private IngridientsRepository ingridientsRepository;
+    @Mock
     private ValidationService validationService;
 
+    @InjectMocks
+    private IngridientsServiceImpl ingridientsService;
     @InjectMocks
     private RecipesServiceImpl recipesService;
 
@@ -38,10 +48,12 @@ public class RecipesServiceImplTest {
         Recipes recipe1 = new Recipes();
         recipe1.setId(1);
         recipe1.setName("Recipe 1");
+        recipe1.setPrice(5000);
 
         Recipes recipe2 = new Recipes();
         recipe2.setId(2);
         recipe2.setName("Recipe 2");
+        recipe2.setPrice(10000);
 
         mockRecipesList = Arrays.asList(recipe1, recipe2);
     }
@@ -64,5 +76,31 @@ public class RecipesServiceImplTest {
         when(repository.findById(1)).thenReturn(Optional.empty());
 
         assertThrows(ResponseStatusException.class, () -> recipesService.getOne(1));
+    }
+    @Test
+    public void createSuccess() {
+        Items items = new Items();
+        items.setId(1);
+        items.setQty(10);
+        Ingridients ing = new Ingridients();
+        ing.setItem(items);
+        ing.setQty(1);
+
+        RecipeCreatesDTO dto = new RecipeCreatesDTO("Recipe 1",
+                "Ayam, Minyak","Gorenglah Ayam",10000,);
+
+        Recipes expectedRecipes = new Recipes();
+        expectedRecipes.setName(dto.getName());
+        expectedRecipes.setPrice(dto.getPrice());
+
+        when(repository.save(any())).thenReturn(expectedRecipes);
+
+        Recipes result = recipesService.create(dto);
+
+        assertNotNull(result);
+        assertEquals("Recipe 1", result.getName());
+        assertEquals(10000, result.getPrice());
+        verify(validationService, times(1)).validate(dto);
+        verify(repository, times(1)).save(any(Recipes.class));
     }
 }
